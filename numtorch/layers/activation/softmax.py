00d2dd8aee245830c0d8d49716afc2e31fc45e35
Slim Beji <mslimbeji@gmail.com>
2025-03-23 10:53:45 +0100

    add input validation to softmax layer

--- numtorch/layers/activation/softmax.py
+++ numtorch/layers/activation/softmax.py
@@ -5,6 +5,16 @@ from numtorch.utils import softmax, softmax_jacobian
 
 
 class SoftmaxActivation(BaseLayer):
+    def _check_input(self, x: np.ndarray) -> np.ndarray:
+        """Accept only np.ndarray with two dimensions"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError(
+                f"Expected input of type np.ndarray, received {type(x)} instead"
+            )
+
+        if x.ndim not in [1, 2]:
+            raise ValueError(f"Expected a 1D or 2D input, received {x.ndim}D instead")
+
     def _forward(self, x: np.ndarray) -> np.ndarray:
         return softmax(x)
 
