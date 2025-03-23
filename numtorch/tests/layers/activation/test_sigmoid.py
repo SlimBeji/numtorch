7097057bd8a3d2747fccf484bd6b5c4f139cd2e5
Slim Beji <mslimbeji@gmail.com>
2025-03-23 16:19:30 +0100

    add unit tests for sigmoid layer

---
+++ numtorch/tests/layers/activation/test_sigmoid.py
@@ -0,0 +1,114 @@
+import numpy as np
+import pytest
+
+from numtorch.layers import SigmoidActivation
+from numtorch.utils.context import eval
+
+
+def test_constructor():
+    layer = SigmoidActivation()
+    assert layer.name == "SigmoidActivation"
+    assert layer.trainable == False
+    layer.name = "Customised name"
+    assert layer.name == "Customised name"
+
+
+def test_input_validation():
+    # Building the layer
+    layer = SigmoidActivation()
+
+    # Type int is not accepted
+    with pytest.raises(
+        TypeError,
+        match="Expected input of type np.ndarray, received <class 'int'> instead",
+    ) as exc:
+        layer(8)
+
+    # Type float is not accepted
+    with pytest.raises(
+        TypeError,
+        match="Expected input of type np.ndarray, received <class 'float'> instead",
+    ) as exc:
+        layer(8.0)
+
+
+def test_forward():
+    # Create Layer
+    layer = SigmoidActivation()
+
+    # First Test for vector
+    vector_1 = np.array([0, np.log(2)])
+    expected_1 = np.array([0.5, 2 / 3])
+    output_1 = layer._forward(vector_1)
+    assert np.allclose(output_1, expected_1)
+    assert np.allclose(layer(vector_1), expected_1)
+
+    # Second Test for vector
+    vector_2 = np.array([np.log(0.5), np.log(0.25)])
+    expected_2 = np.array([1 / 3, 0.2])
+    output_2 = layer._forward(vector_2)
+    assert np.allclose(output_2, expected_2)
+    assert np.allclose(layer(vector_2), expected_2)
+
+    # Test with batch
+    batch_input = np.array([vector_1, vector_2])
+    batch_output = layer._forward(batch_input)
+    batch_expected = np.array([expected_1, expected_2])
+    assert np.allclose(batch_output, batch_expected)
+    assert np.allclose(layer(batch_input), batch_expected)
+
+
+def test_grad_methods():
+    # Create Layer
+    layer = SigmoidActivation()
+
+    # First Test for vector
+    vector_1 = np.array([0, np.log(2)])
+    expected_1 = np.array([[0.25, 0], [0, 2 / 9]])
+    layer(vector_1)
+    assert np.allclose(layer._grad(vector_1), expected_1)
+    assert np.allclose(layer.inputs_grad, expected_1)
+    assert layer.parameters_grad == {}
+
+    # Second Test for vector
+    vector_2 = np.array([np.log(0.5), np.log(0.25)])
+    expected_2 = np.array([[2 / 9, 0], [0, 0.16]])
+    layer(vector_2)
+    assert np.allclose(layer._grad(vector_2), expected_2)
+    assert np.allclose(layer.inputs_grad, expected_2)
+    assert layer.parameters_grad == {}
+
+    # Test with batch
+    batch_input = np.array([vector_1, vector_2])
+    batch_expected = np.array([expected_1, expected_2])
+    layer(batch_input)
+    assert np.allclose(layer._grad(batch_input), batch_expected)
+    assert np.allclose(layer.inputs_grad, batch_expected)
+    assert layer.parameters_grad == {}
+
+
+def test_disabled_training():
+    # Create Layer
+    layer = SigmoidActivation()
+    assert layer.trainable == False
+
+    # Can't make the layer trainable
+    layer.trainable = True
+    assert layer.trainable == False
+
+
+def test_eval_mode():
+    # Create Layer
+    layer = SigmoidActivation()
+    assert layer.trainable == False
+
+    # Testing vector
+    vector = np.array([0, np.log(2)])
+    expected = np.array([0.5, 2 / 3])
+
+    # No grad computation
+    with eval():
+        output = layer(vector)
+    assert np.allclose(output, expected)
+    assert layer.inputs_grad is None
+    assert layer.parameters_grad == {}
