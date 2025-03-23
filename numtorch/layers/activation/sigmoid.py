b9230019252fdce515bb0f72f5b9e278b8150293
Slim Beji <mslimbeji@gmail.com>
2025-03-23 16:19:14 +0100

    check input for sigmoid layer

--- numtorch/layers/activation/sigmoid.py
+++ numtorch/layers/activation/sigmoid.py
@@ -5,6 +5,14 @@ from numtorch.utils import sigmoid, sigmoid_jacobian
 
 
 class SigmoidActivation(BaseLayer):
+    def _check_input(self, x: np.ndarray) -> np.ndarray:
+        """Accept all sort of inputs, provided they are
+        of type np.ndarray"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError(
+                f"Expected input of type np.ndarray, received {type(x)} instead"
+            )
+
     def _forward(self, x: np.ndarray) -> np.ndarray:
         return sigmoid(x)
 
