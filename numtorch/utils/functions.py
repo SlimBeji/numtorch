4e974517b5e9e10b6379cb8135214be7824669a6
Slim Beji <mslimbeji@gmail.com>
2025-03-25 21:07:50 +0100

    linting

--- numtorch/utils/functions.py
+++ numtorch/utils/functions.py
@@ -2,7 +2,8 @@ import numpy as np
 
 
 def sigmoid(x: np.ndarray) -> np.ndarray:
-    return 1/(1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 def sigmoid_prime(x: np.ndarray) -> np.ndarray:
     return sigmoid(x) * (1 - sigmoid(x))
@@ -13,18 +14,22 @@ def sigmoid_jacobian(x: np.ndarray) -> np.ndarray:
 def tanh(x: np.ndarray) -> np.ndarray:
     return np.tanh(x)
 
+
 def tanh_prime(x: np.ndarray) -> np.ndarray:
     return 1 - np.tanh(x) ** 2
+
 
 def tanh_jacobian(x: np.ndarray) -> np.ndarray:
     return np.eye(len(x)) * tanh_prime(x)
 
+
 def softmax(x: np.ndarray) -> np.ndarray:
-    maximums = x.max(axis=-1 , keepdims=True)
+    maximums = x.max(axis=-1, keepdims=True)
     shifted = x - maximums
     exp: np.ndarray = np.exp(shifted)
     exp_sums = exp.sum(axis=-1, keepdims=True)
     return exp / exp_sums
+
 
 def softmax_jacobian(x: np.ndarray) -> np.ndarray:
     f = softmax(x)
@@ -34,7 +39,7 @@ def softmax_jacobian(x: np.ndarray) -> np.ndarray:
     for i in range(n):
         for j in range(n):
             if i == j:
-                result[...,i, j] =  f[..., i] * (1 - f[..., i])
+                result[..., i, j] = f[..., i] * (1 - f[..., i])
             else:
-                result[...,i, j] = -f[..., i] * f[..., j]
+                result[..., i, j] = -f[..., i] * f[..., j]
     return result
