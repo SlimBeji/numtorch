34c99d1f9139b1bc4b90cfbd9b26dc8cb4671afc
Slim Beji <mslimbeji@gmail.com>
2025-03-27 16:00:46 +0100

    fix bug with tanh layer

--- numtorch/layers/activation/tanh.py
+++ numtorch/layers/activation/tanh.py
@@ -5,8 +5,19 @@ from numtorch.utils import tanh, tanh_jacobian
 
 
 class TanhActivation(BaseLayer):
+    def _check_input(self, x: np.ndarray) -> np.ndarray:
+        """Accept all sort of inputs, provided they are
+        of type np.ndarray"""
+        if not isinstance(x, np.ndarray):
+            raise TypeError(
+                f"Expected input of type np.ndarray, received {type(x)} instead"
+            )
+
+        if x.ndim not in [1, 2]:
+            raise ValueError(f"Expected a 1D or 2D input, received {x.ndim}D instead")
+
     def _forward(self, x: np.ndarray) -> np.ndarray:
         return tanh(x)
 
     def _grad(self, x: np.ndarray) -> np.ndarray:
-        return tanh_jacobian
+        return tanh_jacobian(x)
