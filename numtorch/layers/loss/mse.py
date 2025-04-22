5278c4f95701bb6f582488e80c0036f179989899
Slim Beji <mslimbeji@gmail.com>
2025-04-22 15:17:57 +0100

    fix bug in mse loss

--- numtorch/layers/loss/mse.py
+++ numtorch/layers/loss/mse.py
@@ -7,8 +7,19 @@ from numtorch.utils.loss import mse
 class MSELoss(LossLayer):
     IS_TRAINABLE = False
 
+    def _check_input(self, y: np.ndarray, target: np.ndarray):
+        if not isinstance(y, np.ndarray) or not isinstance(target, np.ndarray):
+            raise TypeError(
+                "Expected inputs of type np.ndarray, received ({}, {}) instead".format(
+                    type(y), type(target)
+                )
+            )
+
+        if y.shape != target.shape:
+            raise ValueError("Shape mismatch between preds and targets")
+
     def _forward(self, y: np.ndarray, target: np.ndarray) -> float:
-        return mse(y, target) / 2
+        return mse(y, target)
 
     def _grad(self, y: np.ndarray, target: np.ndarray) -> np.ndarray:
-        return (y - target).mean()
+        return 2 * (y - target) / y.size
